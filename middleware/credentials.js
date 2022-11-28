fe25207cc7adf17a4f026003d8784b55efc33f1f
Nam Nguyen <phuongnam2804@gmail.com>
2022-11-28 10:55:12 +0700

    fix CORS

--- middleware/credentials.js
+++ middleware/credentials.js
@@ -1,10 +1,20 @@
 const allowedOrigins = require("../config/allowedOrigins");
 
 const credentials = (req, res, next) => {
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.header("Access-Control-Allow-Credentials", true);
-  }
+  // const origin = req.headers.origin;
+  // if (allowedOrigins.includes(origin)) {
+  //   res.header("Access-Control-Allow-Credentials", true);
+  // }
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, OPTIONS"
+  );
   next();
 };
 
